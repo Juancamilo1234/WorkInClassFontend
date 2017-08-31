9413c900d5d0527f9be73d04bd932971fd7a22fa
juan camilo salazar <jcsalazar148@misena.edu.co>
2017-08-31 08:14:04 -0500

    update main stylus

---
+++ client/app/app.constant.js
@@ -0,0 +1,13 @@
+(function(angular, undefined) {
+  angular.module("workInClassFontendApp.constants", [])
+
+.constant("appConfig", {
+	"userRoles": [
+		"guest",
+		"user",
+		"admin"
+	]
+})
+
+;
+})(angular);
